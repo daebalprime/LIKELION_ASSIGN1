b81778bf1f2fb5a18ad5c0593d5a1d974b7c4b11
daebalprime <daebalprime@gmail.com>
2019-03-04 00:20:43 +0900

    제발 자자...

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v^weja1cyh#))@skz8%li1wj_wf1dl
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = ['*']
 
 
