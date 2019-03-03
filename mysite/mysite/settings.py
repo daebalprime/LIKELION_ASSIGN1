0d6a0a4f60e2b1bf34a416862039e9c782639484
daebalprime <daebalprime@gmail.com>
2019-03-03 23:55:59 +0900

    제발좀되자...

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v^weja1cyh#))@skz8%li1wj_wf1dl
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
